7af95bb0a1f913cda19ca5a9470f084e5b6c40c3
EnTeR-uSeRnAmEs <2479880643@qq.com>
2023-05-17 11:06:37 +0800

    update

--- Python计算综合实验/Src/MNIST_test.py
+++ Python计算综合实验/Src/MNIST_test.py
@@ -20,21 +20,35 @@ random_seed = 1
 torch.manual_seed(random_seed)
 
 train_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST(path + "/" + data_path, train=True, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                       (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size_train, shuffle=True)
+    torchvision.datasets.MNIST(
+        path + "/" + data_path,
+        train=True,
+        download=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
+    ),
+    batch_size=batch_size_train,
+    shuffle=True,
+)
 test_loader = torch.utils.data.DataLoader(
-    torchvision.datasets.MNIST(path + "/" + data_path, train=False, download=True,
-                               transform=torchvision.transforms.Compose([
-                                   torchvision.transforms.ToTensor(),
-                                   torchvision.transforms.Normalize(
-                                       (0.1307,), (0.3081,))
-                               ])),
-    batch_size=batch_size_test, shuffle=True)
+    torchvision.datasets.MNIST(
+        path + "/" + data_path,
+        train=False,
+        download=True,
+        transform=torchvision.transforms.Compose(
+            [
+                torchvision.transforms.ToTensor(),
+                torchvision.transforms.Normalize((0.1307,), (0.3081,)),
+            ]
+        ),
+    ),
+    batch_size=batch_size_test,
+    shuffle=True,
+)
 
 examples = enumerate(test_loader)
 batch_idx, (example_data, example_targets) = next(examples)
@@ -45,7 +59,7 @@ fig = plt.figure()
 for i in range(6):
     plt.subplot(2, 3, i + 1)
     plt.tight_layout()
-    plt.imshow(example_data[i][0], cmap='gray', interpolation='none')
+    plt.imshow(example_data[i][0], cmap="gray", interpolation="none")
     plt.title("Ground Truth: {}".format(example_targets[i]))
     plt.xticks([])
     plt.yticks([])
@@ -72,8 +86,7 @@ class Net(nn.Module):
 
 
 network = Net()
-optimizer = optim.SGD(network.parameters(),
-                      lr=learning_rate, momentum=momentum)
+optimizer = optim.SGD(network.parameters(), lr=learning_rate, momentum=momentum)
 
 train_losses = []
 train_counter = []
@@ -90,18 +103,23 @@ def train(epoch):
         loss.backward()
         optimizer.step()
         if batch_idx % log_interval == 0:
-            print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(epoch, batch_idx * len(data),
-                                                                           len(train_loader.dataset),
-                                                                           100. * batch_idx /
-                                                                           len(train_loader),
-                                                                           loss.item()))
+            print(
+                "Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}".format(
+                    epoch,
+                    batch_idx * len(data),
+                    len(train_loader.dataset),
+                    100.0 * batch_idx / len(train_loader),
+                    loss.item(),
+                )
+            )
             train_losses.append(loss.item())
-            train_counter.append((batch_idx * 64) +
-                                 ((epoch - 1) * len(train_loader.dataset)))
-            torch.save(network.state_dict(), path +
-                       "/" + data_path + '/model.pth')
-            torch.save(optimizer.state_dict(), path +
-                       "/" + data_path + '/optimizer.pth')
+            train_counter.append(
+                (batch_idx * 64) + ((epoch - 1) * len(train_loader.dataset))
+            )
+            # torch.save(network.state_dict(), path +
+            #            "/" + data_path + '/model.pth')
+            # torch.save(optimizer.state_dict(), path +
+            #            "/" + data_path + '/optimizer.pth')
 
 
 def test():
@@ -111,14 +129,19 @@ def test():
     with torch.no_grad():
         for data, target in test_loader:
             output = network(data)
-            test_loss += F.nll_loss(output, target, reduction='sum').item()
+            test_loss += F.nll_loss(output, target, reduction="sum").item()
             pred = output.data.max(1, keepdim=True)[1]
             correct += pred.eq(target.data.view_as(pred)).sum()
     test_loss /= len(test_loader.dataset)
     test_losses.append(test_loss)
-    print('\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n'.format(
-        test_loss, correct, len(test_loader.dataset),
-        100. * correct / len(test_loader.dataset)))
+    print(
+        "\nTest set: Avg. loss: {:.4f}, Accuracy: {}/{} ({:.0f}%)\n".format(
+            test_loss,
+            correct,
+            len(test_loader.dataset),
+            100.0 * correct / len(test_loader.dataset),
+        )
+    )
 
 
 # train(1)
